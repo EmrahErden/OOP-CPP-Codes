847ce3b2f9b88aeb20cacb6894480dc22e224bca
SoajanII <emraherden@gmail.com>
2015-08-25 20:39:47 +0300

    simple critter class

--- critter.cpp
+++ critter.cpp
@@ -9,7 +9,14 @@ private:
 public:
 	void greet(void) {cout << "Hello! I'm a critter and my hunger level is: " << _hunger << "\n";}
 	void setHunger(int a) {_hunger=a;}
+	critter(int hunger);
 };	//critter class
+
+critter::critter(int hunger)
+{
+	_hunger = hunger;
+	cout << "A new critter has been born!!\n";
+}
 
 int main(void)
 {
